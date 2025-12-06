bc13cf26bb03b83c78431d51d5609bc25b1828dc
syuparn <s.hello.spagetti@gmail.com>
2025-12-06 21:02:26 +0900

    add page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // HACK: fix link path because endpoint of GitHub pages is https://syuparn.github.io/ramenshop/ (not root)
+  base: process.env.GITHUB_PAGES
+    ? ''
+    : './'
 })
